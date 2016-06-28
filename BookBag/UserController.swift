8793903dd44431752301616859f8631e55f2a70c
Jordan Nelson <nelsonjordan20@yahoo.com>
2016-06-28 14:12:50 +0200

    Controller Method headers added

---
+++ BookBag/UserController.swift
@@ -0,0 +1,33 @@
+//
+//  UserController.swift
+//  BookBag
+//
+//  Created by Jordan Nelson on 6/28/16.
+//  Copyright © 2016 Jordan Nelson. All rights reserved.
+//
+
+import Foundation
+
+class UserController {
+    
+    
+    // LOGIN - SIGNUP
+    static func logInUser(email:String, password:String, completion:(errorString:String?)-> Void){
+        
+    }
+    
+    static func signUpUser(email:String, password:String, fullName:String, completion:(errorString:String?)-> Void){
+        
+    }
+    
+    static func logOutUser(completion:(success:Bool) -> Void){
+        
+    }
+    
+    
+    // MY LIBRARY
+    static func fetchMyBooks(completion:([Book]?) -> Void) {
+        
+    }
+    
+}
