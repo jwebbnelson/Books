1fa9e762d87ae8842a88983915432583a528bc6b
Jordan Nelson <nelsonjordan20@yahoo.com>
2016-07-08 12:17:03 -0600

    BookDetail Cells
    Enum, updateWith, and outlets

--- BookBag/BookDetailTableViewCell.swift
+++ BookBag/BookDetailTableViewCell.swift
@@ -8,8 +8,19 @@
 
 import UIKit
 
+enum IconTypes:Int {
+    case Title
+    case Author
+    case ISBN
+    case Notes
+}
+
 class BookDetailTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -21,4 +32,18 @@ class BookDetailTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    func updateCellWithBook(book:Book, row:Int) {
+        switch row {
+        case IconTypes.Title.rawValue:
+            detailLabel.text = book.title
+        case IconTypes.Author.rawValue:
+            detailLabel.text = book.author
+        case IconTypes.ISBN.rawValue:
+            detailLabel.text = "\(book.isbn)"
+        case IconTypes.Notes.rawValue:
+            detailLabel.text = book.notes
+        default:
+            detailLabel.text = ""
+        }
+    }
 }
